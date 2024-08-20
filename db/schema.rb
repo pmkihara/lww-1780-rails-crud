152ec3836e8c4ce3c25e3e772a3e9d263202536f
Patricia Kihara <patricia.kihara@outlook.com>
2024-08-20 09:28:21 -0300

    feat: add address to restaurants

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_20_122026) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_20_122304) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.integer "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
   end
 
 end
